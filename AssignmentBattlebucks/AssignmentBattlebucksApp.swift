9b47324f3d713962f752f1eeedc43794ab0c6ffe
Praveenbkbiet2 <praveen.bkbiet2@gmail.com>
2025-10-02 12:14:08 +0530

    Initial Commit

---
+++ AssignmentBattlebucks/AssignmentBattlebucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentBattlebucksApp.swift
+//  AssignmentBattlebucks
+//
+//  Created by Praveen Verma on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentBattlebucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
